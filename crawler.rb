ce0305ed6ad5df8472b10b3db255913c0be66bfb
Paul Yasi <paul@paulyasi.com>
2016-01-24 12:44:16 -0500

    add loop to parse the first level of urls

--- crawler.rb
+++ crawler.rb
@@ -1,41 +1,69 @@
 require 'mechanize'
 require_relative 'pages'
 
-page = Pages.new()
-page.url = "http://www.coffeebuzz.com"
+starting_url = "http://www.coffeebuzz.com"
 
-def parse_page(page)
+def parse_page(url)
+	puts "---------------------- Parsing URL: #{url}"
+	page = Pages.new()
 	agent = Mechanize.new
-	mech = agent.get(page.url)
+	begin
+		mech = agent.get(url)
 
-	wordhash = {}
-	words = mech.search('body').text.split(' ')
-	words.each do|word|
-		if word.length > 4
-			if wordhash.has_key?(word)
-				wordhash[word] = wordhash[word] + 1
-			else
-				wordhash[word] = 1
+		wordhash = {}
+		begin
+			words = mech.search('body').text.split(' ')
+			words.each do|word|
+				if word.length > 4
+					if wordhash.has_key?(word)
+						wordhash[word] = wordhash[word] + 1
+					else
+						wordhash[word] = 1
+					end
+				end
 			end
+		rescue
+			wordhash = {}
 		end
+
+
+		links = []
+		begin
+			mech.links.each do |link|
+				begin
+					nexturl = mech.uri.merge link.href
+				rescue
+					nexturl = link.href
+				end
+				puts nexturl
+				links.push(nexturl)
+			end
+		rescue
+			links = []
+		end
+
+		begin
+			page.title = mech.title
+		rescue
+			page.title = "No Title"
+		end
+
+		page.description = 'page description'
+		page.links = links
+		page.topics = wordhash.sort_by(&:last).reverse[0..5]
+
+		return page
+	rescue
+		puts "invalid url #{url}"
+		# not a valid url, do nothing
 	end
-
-	links = []
-	mech.links.each do |link|
-		links.push(link.href)
-	end
-
-	page.title = mech.title
-	page.description = 'page description'
-	page.links = links
-	page.topics = wordhash.sort_by(&:last).reverse[0..5]
-
-	return page
 
 end
 
-page = parse_page(page)
+page = parse_page(starting_url)
 puts page.links
-puts page.title
+page.links.each do |url|
+    new_page = parse_page(url)
+end
 
 
