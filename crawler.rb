fed603c9250c0b3ff2c742e490b2ec408042f723
Paul Yasi <paul@paulyasi.com>
2016-03-30 20:07:22 -0400

    fill global wordhash var

--- crawler.rb
+++ crawler.rb
@@ -1,7 +1,39 @@
+#!/usr/bin/env ruby
+# a web crawler experiment
+# determine topic of content by crawling title of internal pages of website
+
 require 'mechanize'
 require_relative 'pages'
 
 starting_url = "http://www.coffeebuzz.com"
+$wordhash = {}
+
+def get_title(url)
+	agent = Mechanize.new
+	mech = agent.get(url)
+	mech.title
+
+  begin
+    words = mech.search('body').text.split(' ')
+    puts "BODY"
+    puts words
+    words.each do|word|
+      if word.length > 3
+        puts "get_title words"
+        puts word
+        if $wordhash.has_key?(word)
+          $wordhash[word] = $wordhash[word] + 1
+          puts "w+1"
+        else
+          $wordhash[word] = 1
+        end
+      end
+    end
+  rescue
+    # oops
+  end
+
+end
 
 def parse_page(url)
 	puts "---------------------- Parsing URL: #{url}"
@@ -10,28 +42,44 @@ def parse_page(url)
 	begin
 		mech = agent.get(url)
 
-		wordhash = {}
+		#wordhash = {}
 		begin
 			words = mech.search('body').text.split(' ')
 			words.each do|word|
-				if word.length > 4
-					if wordhash.has_key?(word)
-						wordhash[word] = wordhash[word] + 1
+				if word.length > 3
+          		puts "word:"
+          		puts word
+					if $wordhash.has_key?(word)
+						$wordhash[word] = $wordhash[word] + 1
+						puts "w+1"
 					else
-						wordhash[word] = 1
+						$wordhash[word] = 1
 					end
 				end
 			end
 		rescue
-			wordhash = {}
+			# oops
 		end
 
+    titlehash = {}
 
 		links = []
 		begin
 			mech.links.each do |link|
 				begin
 					nexturl = mech.uri.merge link.href
+					titlewords = get_title(nexturl)
+          words = titlewords.split(' ')
+          words.each do|word|
+            if word.length > 3
+              puts word
+              if titlehash.has_key?(word)
+                titlehash[word] = titlehash[word] + 1
+              else
+                titlehash[word] = 1
+              end
+            end
+          end
 				rescue
 					nexturl = link.href
 				end
@@ -50,9 +98,10 @@ def parse_page(url)
 
 		page.description = 'page description'
 		page.links = links
-		page.topics = wordhash.sort_by(&:last).reverse[0..5]
+		page.topics = titlehash.sort_by(&:last).reverse[0...10]
 
 		return page
+
 	rescue
 		puts "invalid url #{url}"
 		# not a valid url, do nothing
@@ -61,9 +110,14 @@ def parse_page(url)
 end
 
 page = parse_page(starting_url)
-puts page.links
-page.links.each do |url|
-    new_page = parse_page(url)
-end
+puts page.title
+puts page.topics
+puts "wordhash:"
+puts $wordhash
+
+# puts page.links
+# page.links.each do |url|
+#     new_page = parse_page(url)
+# end
 
 
